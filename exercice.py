3bf5e969fcedfcb9bbe9848377de856bea93a47d
Charles Hosson <charles.hosson@polymtl.ca>
2020-09-17 16:17:23 -0400

    Removed answers

--- exercice.py
+++ exercice.py
@@ -4,35 +4,19 @@
 import random
 
 def is_even_len(string):
-	return len(string) % 2 == 0
+	return 0xDEADBEEF
 
 
 def get_num_char(string, char):
-	num_char = 0
-	for c in string:
-		num_char += 1 if c == char else 0
-	return num_char
+	return 0xDEADBABE
 
 
 def get_first_part_of_name(name):
-	first_part = name.split("-")[0]
-	capitalized = first_part[0].upper() + first_part[1:]
-	return "Bonjour, " + capitalized
+	return ""
 
 
 def get_random_sentence(animals, adjectives, fruits):
-	basic_sentence = "Aujourd’hui, j’ai vu un %s s’emparer d’un panier %s plein de %s."
-
-	animal_word = animals[random.randrange(0, len(animals))]
-	adjective_word = adjectives[random.randrange(0, len(adjectives))]
-	fruit_word = fruits[random.randrange(0, len(fruits))]
-	return basic_sentence % (animal_word, adjective_word, fruit_word)
-
-	# Approche sans répétition de code
-	#words = []
-	#for word_set in (animals, adjectives, fruits):
-	#	words += [word_set[random.randrange(0, len(word_set))]]
-	#return basic_sentence % tuple(words)
+	return ""
 
 
 if __name__ == "__main__":
