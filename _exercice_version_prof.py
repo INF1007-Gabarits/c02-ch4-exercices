a1b84c31ef7b784fee387d982d78f248dbc4b398
Charles Hosson <charles.hosson@polymtl.ca>
2020-09-17 14:24:14 -0400

    Added VS files and changed tab indent.

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -2,26 +2,26 @@
 # -*- coding: utf-8 -*-
 
 def capitaliser_pays(nom):
-    # Ceci est la version pour le/la professeur(e)
-    # TODO completer la fonction
-    return nom
+	# Ceci est la version pour le/la professeur(e)
+	# TODO completer la fonction
+	return nom
 
 
 if __name__ == '__main__':
-    pays = [
-        'AfghanIstan',
-        'albania',
-        'algeria',
-        'AndorRa',
-        'angolA',
-        'antigua ANd barbuda',
-        'argEntina',
-        'Armenia',
-        'austrAlia',
-        'ausTria',
-        'azerBaijaN'
-    ]
-    for i in range(len(pays)):
-        pays[i] = capitaliser_pays(pays[i])
+	pays = [
+		'AfghanIstan',
+		'albania',
+		'algeria',
+		'AndorRa',
+		'angolA',
+		'antigua ANd barbuda',
+		'argEntina',
+		'Armenia',
+		'austrAlia',
+		'ausTria',
+		'azerBaijaN'
+	]
+	for i in range(len(pays)):
+		pays[i] = capitaliser_pays(pays[i])
 
-    print(pays)
+	print(pays)
